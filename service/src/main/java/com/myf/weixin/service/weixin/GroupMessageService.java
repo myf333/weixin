660756b05612275c0f1cece489f2062a7f34b2ab
maoyanfeng <myf333@gmail.com>
2016-06-01 18:15:17 +0800

    add

--- service/src/main/java/com/myf/weixin/service/weixin/GroupMessageService.java
+++ service/src/main/java/com/myf/weixin/service/weixin/GroupMessageService.java
@@ -6,7 +6,6 @@ import com.myf.weixin.entity.weixin.groupmsg.*;
 import com.myf.weixin.entity.weixin.media.MaterialNew;
 import com.myf.weixin.entity.weixin.media.MediaUploadRet;
 import com.myf.weixin.util.HttpUtil;
-import org.apache.http.protocol.HTTP;
 
 import java.text.MessageFormat;
 import java.util.List;
@@ -49,17 +48,17 @@ public class GroupMessageService {
     }
 
     /**
-     * 根据标签进行群发【订阅号与服务号认证后均可用】-除文本内容外
+     * 根据标签进行群发【订阅号与服务号认证后均可用】
     * **/
     public static GroupMessageRet sendGroupMessageToAll(String accessToken,boolean isToAll,int tagId,
-                                                        GroupMessageType type,String mediaId)throws Exception{
+                                                        GroupMessageType type,String mediaIdOrText)throws Exception{
         String url = MessageFormat.format("https://api.weixin.qq.com/cgi-bin/message/mass/sendall?access_token={0}",accessToken);
         GroupMessageRequest request = new GroupMessageRequest();
         GroupFilter filter = new GroupFilter();
         filter.setIs_to_all(isToAll);
         filter.setTag_id(tagId);
         MediaContent content = new MediaContent();
-        content.setMedia_id(mediaId);
+        content.setMedia_id(mediaIdOrText);
         switch (type)
         {
             case mpnews:
@@ -69,7 +68,13 @@ public class GroupMessageService {
                 request = mpnews;
                 break;
             case text:
-                throw new Exception("请使用文本发送接口发送群发文本消息");
+                TextRequest text = new TextRequest();
+                text.setFilter(filter);
+                TextContent textContent = new TextContent();
+                textContent.setContent(mediaIdOrText);
+                text.setText(textContent);
+                request = text;
+                break;
             case voice:
                 VoiceRequest voice = new VoiceRequest();
                 voice.setFilter(filter);
@@ -92,7 +97,7 @@ public class GroupMessageService {
                 MpWxCardRequest wxcard = new MpWxCardRequest();
                 wxcard.setFilter(filter);
                 WxCardContent card = new WxCardContent();
-                card.setCard_id(mediaId);
+                card.setCard_id(mediaIdOrText);
                 wxcard.setWxcard(card);
                 request = wxcard;
                 break;
@@ -104,13 +109,13 @@ public class GroupMessageService {
     }
 
     /**
-     * 根据OpenId分组进行群发【订阅号不可用，服务号认证后可用】-除文本内容外
+     * 根据OpenId分组进行群发【订阅号不可用，服务号认证后可用
      * **/
-    public static GroupMessageRet sendGroupMessageByOpenId(String accessToken,List<String> openIds, GroupMessageType type,String mediaId)throws Exception{
+    public static GroupMessageRet sendGroupMessageByOpenId(String accessToken,List<String> openIds, GroupMessageType type,String mediaIdOrText)throws Exception{
         String url = MessageFormat.format("https://api.weixin.qq.com/cgi-bin/message/mass/send?access_token={0}",accessToken);
         GroupMessageRequest request = new GroupMessageRequest();
         MediaContent content = new MediaContent();
-        content.setMedia_id(mediaId);
+        content.setMedia_id(mediaIdOrText);
         switch (type)
         {
             case mpnews:
@@ -120,7 +125,13 @@ public class GroupMessageService {
                 request = mpnews;
                 break;
             case text:
-                throw new Exception("请使用文本发送接口发送群发文本消息");
+                TextOpenIdRequest<List<String>> text = new TextOpenIdRequest<>();
+                TextContent textContent = new TextContent();
+                textContent.setContent(mediaIdOrText);
+                text.setText(textContent);
+                text.setTouser(openIds);
+                request = text;
+                break;
             case voice:
                 VoiceOpenIdRequest<List<String>> voice = new VoiceOpenIdRequest<>();
                 voice.setTouser(openIds);
@@ -143,55 +154,15 @@ public class GroupMessageService {
                 MpWxCardOpenIdRequest<List<String>> wxcard = new MpWxCardOpenIdRequest<>();
                 wxcard.setTouser(openIds);
                 WxCardContent card = new WxCardContent();
-                card.setCard_id(mediaId);
+                card.setCard_id(mediaIdOrText);
                 wxcard.setWxcard(card);
                 request = wxcard;
                 break;
         }
         request.setMsgtype(type);
         Gson gson = new Gson();
-        String res = HttpUtil.postJson(url,gson.toJson(request));
-        return gson.fromJson(res,GroupMessageRet.class);
-    }
-
-    /**
-     * 根据标签(分组)进行群发文本消息【订阅号与服务号认证后均可用
-     * **/
-    public static GroupMessageRet sendTextGroupMessageToAll(String accessToken,boolean isToAll,int tagId,String text)throws Exception{
-        String url = MessageFormat.format("https://api.weixin.qq.com/cgi-bin/message/mass/sendall?access_token={0}",accessToken);
-        TextRequest request = new TextRequest();
-
-        GroupFilter filter = new GroupFilter();
-        filter.setIs_to_all(isToAll);
-        filter.setTag_id(tagId);
-        request.setFilter(filter);
-
-        TextContent content = new TextContent();
-        content.setContent(text);
-        request.setText(content);
-
-        request.setMsgtype(GroupMessageType.text);
-
-        Gson gson = new Gson();
-        String res = HttpUtil.postJson(url,gson.toJson(request));
-        return gson.fromJson(res,GroupMessageRet.class);
-    }
-    /**
-     * 根据OpenID列表群发【订阅号不可用，服务号认证后可用】
-     * **/
-    public static GroupMessageRet sendTextGroupMessageByOpendId(String accessToken,List<String> openIds,String text)throws Exception{
-        String url = MessageFormat.format("https://api.weixin.qq.com/cgi-bin/message/mass/send?access_token={0}",accessToken);
-        TextOpenIdRequest<List<String>> request = new TextOpenIdRequest<>();
-        request.setTouser(openIds);
-
-        TextContent content = new TextContent();
-        content.setContent(text);
-        request.setText(content);
-
-        request.setMsgtype(GroupMessageType.text);
-
-        Gson gson = new Gson();
-        String res = HttpUtil.postJson(url,gson.toJson(request));
+        String json = gson.toJson(request);
+        String res = HttpUtil.postJson(url,json);
         return gson.fromJson(res,GroupMessageRet.class);
     }
 
@@ -202,20 +173,68 @@ public class GroupMessageService {
      * touser 接收消息用户对应该公众号的openid，该字段也可以改为towxname，以实现对微信号的预览
      * touser字段都可以改为towxname，这样就可以针对微信号进行预览（而非openID），towxname和touser同时赋值时，以towxname优先
      * **/
-    public static GroupMessageRet preview(String accessToken,String touser,String towxname,String text) throws Exception{
+    public static GroupMessageRet preview(String accessToken,String touser,String towxname,GroupMessageType type,String mediaIdOrText) throws Exception{
         String url = MessageFormat.format("https://api.weixin.qq.com/cgi-bin/message/mass/preview?access_token={0}",accessToken);
-        TextOpenIdRequest<String> request = new TextOpenIdRequest<>();
-        request.setTouser(touser);
-        request.setTowxname(towxname);
-        TextContent content = new TextContent();
-        content.setContent(text);
-        request.setText(content);
-        request.setMsgtype(GroupMessageType.text);
+        GroupMessageRequest request = new GroupMessageRequest();
+        MediaContent content = new MediaContent();
+        content.setMedia_id(mediaIdOrText);
+        switch (type)
+        {
+            case mpnews:
+                MpnewsOpenIdRequest<String> mpnews = new MpnewsOpenIdRequest<>();
+                mpnews.setTouser(touser);
+                mpnews.setTowxname(towxname);
+                mpnews.setMpnews(content);
+                request = mpnews;
+                break;
+            case text:
+                TextOpenIdRequest<String> text = new TextOpenIdRequest<>();
+                TextContent textContent = new TextContent();
+                textContent.setContent(mediaIdOrText);
+                text.setText(textContent);
+                text.setTouser(touser);
+                text.setTowxname(towxname);
+                request = text;
+                break;
+            case voice:
+                VoiceOpenIdRequest<String> voice = new VoiceOpenIdRequest<>();
+                voice.setTouser(touser);
+                voice.setVoice(content);
+                voice.setTowxname(towxname);
+                request = voice;
+                break;
+            case image:
+                ImageOpenIdRequest<String> image = new ImageOpenIdRequest<>();
+                image.setTouser(touser);
+                image.setImage(content);
+                image.setTowxname(towxname);
+                request = image;
+                break;
+            case mpvideo:
+                MpVideoOpenIdRequest<String> mpvideo = new MpVideoOpenIdRequest<>();
+                mpvideo.setTouser(touser);
+                mpvideo.setMpvideo(content);
+                mpvideo.setTowxname(towxname);
+                request = mpvideo;
+                break;
+            case wxcard:
+                MpWxCardOpenIdRequest<String> wxcard = new MpWxCardOpenIdRequest<>();
+                wxcard.setTouser(touser);
+                WxCardContent card = new WxCardContent();
+                card.setCard_id(mediaIdOrText);
+                wxcard.setWxcard(card);
+                wxcard.setTowxname(towxname);
+                request = wxcard;
+                break;
+        }
+        request.setMsgtype(type);
         Gson gson = new Gson();
         String json = gson.toJson(request);
         String res = HttpUtil.postJson(url,json);
         return gson.fromJson(res,GroupMessageRet.class);
     }
+
+
 
     /**
      * 查询群发消息发送状态【订阅号与服务号认证后均可用】
--- web/src/test/java/com/myf/weixin/test/GroupMessageTest.java
+++ web/src/test/java/com/myf/weixin/test/GroupMessageTest.java
@@ -1,14 +1,22 @@
 package com.myf.weixin.test;
 
+import com.google.gson.Gson;
 import com.myf.weixin.entity.weixin.groupmsg.GroupMessageRet;
+import com.myf.weixin.entity.weixin.groupmsg.GroupMessageStatus;
+import com.myf.weixin.entity.weixin.groupmsg.GroupMessageType;
 import com.myf.weixin.service.weixin.AccessTokenService;
 import com.myf.weixin.service.weixin.GroupMessageService;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by myf on 2016/6/1.
@@ -19,12 +27,38 @@ public class GroupMessageTest {
     @Autowired
     private AccessTokenService tokenService;
 
+    final Logger logger  =  LoggerFactory.getLogger(getClass());
+
     @Test
     public void testPreview() throws Exception{
         String appId = "wxc3063aee2db008c0";
         String appSecret="b4b58f2cdecc3037ac56704796275a37";
         String accessToken = tokenService.GetAccessToken(appId,appSecret);
-        GroupMessageRet ret = GroupMessageService.preview(accessToken,"o-SzhjpT1RWYi170nd9LwKXHj7-o",null,"你好呀，这是一个群发测试");
+        GroupMessageRet ret = GroupMessageService.preview(accessToken,"o-SzhjpT1RWYi170nd9LwKXHj7-o",null,GroupMessageType.text,"你好呀，这是一个群发测试2");
         Assert.assertNotNull(ret);
+    }
+
+    @Test
+    public void testSendByOpenId()throws Exception{
+        String appId = "wxc3063aee2db008c0";
+        String appSecret="b4b58f2cdecc3037ac56704796275a37";
+        String accessToken = tokenService.GetAccessToken(appId,appSecret);
+        List<String> list = new ArrayList<String>();
+        list.add("o-SzhjpT1RWYi170nd9LwKXHj7-o");
+        list.add("o-Szhjr0s1R1iDkJJeXj7CnM8Zlk");
+        String mediaId = "LyWpygZCpIcEYEu1NXlg9FIHzmmgVKLocmjn2feToPdCGSLj2JiG05oTYMS9rJpK";//必须为有效的媒体id，不然会报system error
+        GroupMessageRet ret = GroupMessageService.sendGroupMessageByOpenId(accessToken,list, GroupMessageType.image,mediaId);
+        Gson gson = new Gson();
+        logger.info(gson.toJson(ret));
+        Assert.assertNotNull(ret);
+    }
+
+    @Test
+    public void testSendStatus() throws Exception{
+        String appId = "wxc3063aee2db008c0";
+        String appSecret="b4b58f2cdecc3037ac56704796275a37";
+        String accessToken = tokenService.GetAccessToken(appId,appSecret);
+        GroupMessageStatus res = GroupMessageService.getSendStatus(accessToken,2555967676L);
+        Assert.assertNotNull(res);
     }
 }
